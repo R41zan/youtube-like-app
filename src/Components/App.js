a62bd3e571d22b17ed7e74ecbc46cbfa9edad895
R41zan <pinho262@gmail.com>
2019-05-30 21:17:47 +0100

    Small preparations

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import MainVideo from "./MainVideo";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div className="ui container">
+        <SearchBar />
+        <MainVideo />
+      </div>
+    );
   }
 }
 
---
+++ src/Components/MainVideo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class MainVideo extends React.Component{
+    render(){
+        return(
+            <div>Hello</div>
+        );
+    }
+}
+export default MainVideo;
